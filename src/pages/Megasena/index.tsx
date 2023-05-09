48b8b03cd92df35bf0d8c2fa0ae10dfd11332073
Rodrigo Diniz Goulart <rodinizgoulart@gmail.com>
2023-05-08 23:29:41 -0300

    style: fix component Principal folder name

--- src/pages/Megasena/index.tsx
+++ src/pages/Megasena/index.tsx
@@ -1,6 +1,6 @@
 import { useContexto } from "../../hooks";
 import { Carregando } from "../../components/Carregando";
-import Principal from "../../components/P`rincipal";
+import Principal from "../../components/Principal";
 
 export default function Megasena() {
   const { megasena } = useContexto();
